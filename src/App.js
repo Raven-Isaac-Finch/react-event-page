34839e149188151a45997952f93ab69f38f34909
Raven I. Finch <purplebox02@gmail.com>
2022-10-02 16:06:19 +0300

    Bug Fix for Live site

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/pages/LandingPage';
 import EventPage from './components/pages/EventPage';
